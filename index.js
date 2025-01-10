1540b33bc84bbec1e0c8bea8875cf5ed2989125b
AKOHb <toshick9@gmail.com>
2025-01-10 15:33:48 +0300

    feat: update time func

--- index.js
+++ index.js
@@ -18,13 +18,34 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const validData = data => data.toUTCString() === "Invalid Date";
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+
+  let data = new Date(req.params.date);
+
+  if ( validData(data) ) {
+    data = new Date(+req.params.date);
+  }
+  
+  if ( validData(data) ) {
+    res.json({error : "Invalid Date"});
+    return;
+  }
+
+  res.json({
+    unix: data.getTime(), 
+    utc: data.toUTCString()
+  });
 });
 
-
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(), 
+    utc: new Date().toUTCString()
+  })
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
